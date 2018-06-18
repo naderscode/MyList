1adf306971cf9896499aeb83246ed2ad8adf594b
naderscode <naderscode@gmail.com>
2018-06-18 14:20:28 -0700

    update index.html file

---
+++ app/main.js
@@ -0,0 +1,4 @@
+"use strict";
+var platform_broeser_dynamic_1 = require('@angular/platform-broeser-dynamic');
+var app_module_1 = require('./app.module');
+platform_broeser_dynamic_1.platformBrowserDynamic().bootstrap.Module(app_module_1.AppModule);
