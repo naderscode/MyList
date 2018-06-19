1a32ebf6144d212db4358a9e9f65a9e449207977
naderscode <naderscode@gmail.com>
2018-06-19 08:50:50 -0700

    update main.ts

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrap.Module(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule);
