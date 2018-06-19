8d7be17eaa6244ed1b85d28988595305189a8b72
naderscode <naderscode@gmail.com>
2018-06-19 08:42:06 -0700

    update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,11 @@
-@import { NgModule } from "@angular/core";
+import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 
-@Ngmodule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+@NgModule({
+    imports: [BrowserModule, FormsModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
