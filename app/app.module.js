dab849b5c873cb2630b4edf73e062b5eca70296d
naderscode <naderscode@gmail.com>
2018-06-19 08:46:21 -0700

    update app.module.js

--- app/app.module.js
+++ app/app.module.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
 var app_component_1 = require("./app.component");
@@ -15,7 +16,7 @@ var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
-        NgModule({
+        core_1.NgModule({
             imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
             declarations: [app_component_1.AppComponent],
             bootstrap: [app_component_1.AppComponent]
